7125be5d6ee155fc6b00f4780eb8ed2837813793
agouliel_imac <agouliel@gmail.com>
2022-03-31 15:50:49 +0300

    added noon job

---
+++ JOB_Approve_noon_reports.sql
@@ -0,0 +1,27 @@
+USE msdb;
+GO  
+EXEC dbo.sp_add_job  
+    @job_name = N'Approve noon reports';
+GO  
+EXEC sp_add_jobstep  
+    @job_name = N'Approve noon reports',  
+    @step_name = N'Step 1',  
+    @subsystem = N'TSQL',  
+    @command = N'EXECUTE SP_Approve_noon_reports',
+    @retry_attempts = 5,  
+    @retry_interval = 5 ;  
+GO  
+EXEC dbo.sp_add_schedule  
+    @schedule_name = N'RunEvery30Minutes',
+    @freq_type = 4,
+    @freq_interval = 1,
+    @freq_subday_type = 4,
+    @freq_subday_interval = 30;
+GO  
+EXEC sp_attach_schedule
+   @job_name = N'Approve noon reports',  
+   @schedule_name = N'RunEvery20Minutes';
+GO  
+EXEC dbo.sp_add_jobserver  
+    @job_name = N'Approve noon reports';  
+GO  
